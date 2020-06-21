7da67e1941ff119b85d0f3eb0fa4d5f656324366
iheb-chakroun <47356245+iheb-chakroun@users.noreply.github.com>
2020-06-21 16:55:28 +0100

    Update SummaryPage.js

--- client/src/views/app/Quiz/SummaryPage.js
+++ client/src/views/app/Quiz/SummaryPage.js
@@ -1,28 +1,30 @@
-import React, { Component, Fragment } from "react"
-import { Button, Card, CardSubtitle, CardBody, CardTitle } from "reactstrap"
-import { ceil } from "mathjs"
-import { Colxx } from "../../../components/common/CustomBootstrap"
-import PropTypes from "prop-types"
-import { connect } from "react-redux"
-import { Progress } from "reactstrap"
-import * as quizAction from "../../../redux/actions/quiz.actions"
-import { bindActionCreators } from "redux"
+import React, { Component, Fragment } from "react";
+import { Button, Card, CardSubtitle, CardBody, CardTitle } from "reactstrap";
+import { ceil } from "mathjs";
+import { Colxx } from "../../../components/common/CustomBootstrap";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { Progress } from "reactstrap";
+import * as quizAction from "../../../redux/actions/quiz.actions";
+import * as profileActions from "../../../redux/actions/profile.actions";
 
-import GradientWithRadialProgressCard from "../../../components/cards/GradientWithRadialProgressCard"
+import { bindActionCreators } from "redux";
+
+import GradientWithRadialProgressCard from "../../../components/cards/GradientWithRadialProgressCard";
 
 export class SummaryPage extends Component {
   constructor(props, context) {
-    super(props, context)
+    super(props, context);
     this.state = {
       score: 0,
       numberofQuestions: 0,
       correctAnswers: 0,
       wrongAnswers: 0,
       nbNoAnswer: 0,
-    }
+    };
   }
   componentDidMount() {
-    const { state } = this.props.location
+    const { state } = this.props.location;
 
     this.setState({
       score: (state.score / state.numberofQuestions) * 100,
@@ -30,33 +32,33 @@ export class SummaryPage extends Component {
       correctAnswers: state.correctAnswers,
       wrongAnswers: state.wrongAnswers,
       nbNoAnswer: state.nbNoAnswer,
-    })
+    });
   }
 
   handleQuit = () => {
-    window.confirm("Are you sure you want to quite?")
-    let user =JSON.parse(localStorage.getItem('user'))
-    let score =ceil((this.props.location.state.score * 100) / this.props.location.state.totalScore)
-    let skill= this.props.location.state.skill
-    console.log("the score"+score)
-    let grade= ceil((score/100) *20 )
-    console.log(user.id)
-    console.log("le grade"+grade)
-      if(grade>10){
+    window.confirm("Are you sure you want to quite?");
+    let user = JSON.parse(localStorage.getItem("user"));
+    let score = ceil(
+      (this.props.location.state.score * 100) /
+        this.props.location.state.totalScore
+    );
+    let skill = this.props.location.state.skill;
+    console.log("the score" + score);
+    let grade = ceil((score / 100) * 20);
+    console.log(user.id);
+    console.log("le grade" + grade);
+    if (grade > 10) {
+      this.props.actions.assignSkill(user.id, skill, grade)
+      .then(() => {
+        this.props.profileActions.fetchProfile(user.id)
+      })
+      .catch((e) => {
+        alert(e);
+      });
+    }
 
-        this.props.actions.assignSkill(user.id,skill,grade).catch((e) => {
-          alert(e)
-        })
-      }
-      
-
-
-
-
-
-
-    this.props.history.push("/app/Quiz/quizzes")
-  }
+    this.props.history.push("/app/Quiz/quizzes");
+  };
 
   render() {
     return (
@@ -116,23 +118,24 @@ export class SummaryPage extends Component {
           </Card>
         </CardBody>
       </Fragment>
-    )
+    );
   }
 }
 SummaryPage.propType = {
   dispatch: PropTypes.func.isRequired,
   actions: PropTypes.array.isRequired,
-}
+};
 
 function mapStateToProps(state) {
-
   return {
-    quiz: state.quiz
-  }
+    quiz: state.quiz,
+  };
 }
 
 function mapDispatchtoProps(dispatch) {
-
-  return { actions: bindActionCreators(quizAction, dispatch) }
+  return {
+    actions: bindActionCreators(quizAction, dispatch),
+    profileActions: bindActionCreators(profileActions, dispatch),
+  };
 }
-export default connect(mapStateToProps, mapDispatchtoProps)(SummaryPage)
+export default connect(mapStateToProps, mapDispatchtoProps)(SummaryPage);
